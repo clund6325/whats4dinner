80f61ceb1f111da758e1bb7265f729bcc70b3809
Leticia Fernandes <leticiatotaa@gmail.com>
2021-07-10 10:19:36 -0600

    recipes controller done

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,2 +1,48 @@
 class Api::RecipesController < ApplicationController
+  before_action :set_meal_plan
+  before_action :set_recipe, only: [:show, :update, :destroy]
+
+  def index
+    render json: @meal_plan.recipes
+  end
+
+  def show
+    render json: @recipe
+  end
+
+  def create
+    @recipe = @meal_plan.recipe.new(recipe_params)
+    if @recipe.save
+      render json: @recipe
+    else
+      render json: {errors: @recipe.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      render json: @recipe
+    else
+      render json: {errors: @recipe.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    render json: {message: 'Recipe deleted'}
+  end
+
+  private
+    def set_meal_plan
+      @meal_plan = Meal_plan.find(params[:meal_plan_id])
+    end
+
+    def set_recipe
+      @recipe = @meal_plan.recipes.find(params[:id])
+    end
+
+    def recipe_params
+      params.require(:recipe).permit(:title, :decription, :serv_size)
+    end
 end
+
