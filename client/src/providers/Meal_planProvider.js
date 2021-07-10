849882d19267eda545c8c8ad62baa67278c99036
Leticia Fernandes <leticiatotaa@gmail.com>
2021-07-10 11:17:21 -0600

    ingredient and meal_plan providers done

---
+++ client/src/providers/Meal_planProvider.js
@@ -0,0 +1,62 @@
+import React, {useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const Meal_planContext =  React.createContext();
+
+export const Meal_planConsumer = Meal_planContext.Consumer;
+
+const Meal_planProvider = ({ children }) => {
+
+    const [meal_plan, setMeal_plan] = useState([])
+    useEffect( () => {
+        axios.get('/api/meal_plans')
+        .then( res => setMeal_plans(res.data))
+        .catch(err => console.log(err))
+    }, [])
+
+   const addMeal_plan = (meal_plan) => {
+       axios.post('/api/meal_plans', { meal_plan })
+        .then( res => {
+            setMeal_plans([...meal_plans, res.data])
+        })
+        .catch( err => console.log(err))
+   }
+
+   const updateMeal_plan = (id, meal_plan) => {
+       axios.put(`/api/meal_plans/${id}`, { meal_plan })
+        .then(res => {
+            const updatedMeal_plans = meal_plans.map( m => {
+                if (m.id === id) {
+                    return res.data
+                }
+                return m
+            })
+            setMeal_plans(updatedMeal_plans)
+        })
+        .catch( err => console.log(err) )
+   }
+
+   const deleteMeal_plan = (id) => {
+       axios.delete(`/api/meal_plans/${id}`)
+        .then( res => {
+            setMeal_plans(meal_plans.filter(m => m.id !== id))
+            alert(res.data.message)
+        })
+        .catch( err => console.log(err) )
+
+   }
+   
+    return (
+        <Meal_planContext.Provider value={{
+            meal_plans, 
+            addMeal_plan: addMeal_plan,
+            updateMeal_plan: updateMeal_plan,
+            deleteMeal_plan: deleteMeal_plan,
+
+
+        }}>
+            { children }
+        </Meal_planContext.Provider>
+    )
+}
+export default Meal_planProvider;
