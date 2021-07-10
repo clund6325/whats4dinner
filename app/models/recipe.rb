4208fd6088c97100310437d64c6792dd8525f671
Leticia Fernandes <leticiatotaa@gmail.com>
2021-07-10 15:35:11 -0600

    data seeded

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :meal_plan
-  has_many :ingredients, depedent: :destroy
+  has_many :ingredients, dependent: :destroy
 
   validates :title, :description, :serv_size, presence: true
 end
---
+++ db/seeds.rb
@@ -0,0 +1,36 @@
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+5.times do
+
+    # user = User.create(
+    #   password = password,
+    #   email: Faker::Internet.email
+    # )
+
+    meal_plan = MealPlan.create(
+      week_day: days.sample,
+      meal: Faker::Restaurant.type,
+      theme: Faker::Nation.nationality,
+      user_id: 1
+    )
+
+    3.times do
+        recipe = Recipe.create( 
+        title: Faker::Food.dish,
+        description: Faker::Food.description,
+        serv_size: Faker::Number.between(from: 1, to: 10),
+        meal_plan_id: meal_plan.id
+      )
+
+      2.times do
+        Ingredient.create(
+            food_name: Faker::Food.ingredient,
+            measurement: Faker::Food.measurement,
+            calories: Faker::Address.building_number,
+            recipe_id: recipe.id
+        )
+      end
+    end
+  end
+
+  puts "Data seeded"
