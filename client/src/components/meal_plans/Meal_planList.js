db8b57dec2f393b4e9cdbb626d3cd24dba18a175
Leticia Fernandes <leticiatotaa@gmail.com>
2021-07-10 13:54:38 -0600

    meal_plan show,index,form, and list done

---
+++ client/src/components/meal_plans/Meal_planList.js
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { Meal_planConsumer } from '../../providers/Meal_planProvider';
+import { ListGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Meal_planList = ({ meal_plans, getAllMeal_plans }) => {
+  useEffect( () => {
+    getAllMeal_plans()
+  }, [])
+  return (
+    <>
+      <ListGroup>
+        { meal_plans.map( m => 
+          <Link to={{
+            pathname: `/meal_plans/${m.id}`,
+            state: { ...m }
+          }}>
+            <ListGroup.Item>Meal_plan during: {m.week_day}</ListGroup.Item>
+          </Link>
+        )}
+      </ListGroup>
+    </>
+  )
+}
+const ConnectedMeal_planList = (props) => (
+  <Meal_planConsumer>
+    { value => 
+      <Meal_planList {...props} {...value} />
+    }
+  </Meal_planConsumer>
+)
+export default ConnectedMeal_planList;
